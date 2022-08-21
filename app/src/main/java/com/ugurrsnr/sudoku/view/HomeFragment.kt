3ff1d4011c247fadc79b9efcd6edb1017c5027f9
Ugur <ugurr.snr@gmail.com>
2022-08-21 21:44:20 +0300

    9x9 square box drawed. ontouch event implemented.

--- app/src/main/java/com/ugurrsnr/sudoku/view/HomeFragment.kt
+++ app/src/main/java/com/ugurrsnr/sudoku/view/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.ugurrsnr.sudoku.R
 import com.ugurrsnr.sudoku.adapter.GameLevelAdapter
